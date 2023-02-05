3bd93c0d5d490142a42dcb2122f6ba524db202f1
John Park <58985617+aznjp@users.noreply.github.com>
2023-02-05 13:24:09 -0400

    Updated the makeCacheMatrix and cacheSolve function
    * utilized previous commit additions to update the current functions
    * comments added on how we did this
    * added in tests to verify final work

--- cachematrix.R
+++ cachematrix.R
@@ -46,12 +46,48 @@
 # otherwise it will grab the data of the matrix and then add them to the setmean column of said list
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      inverseMatrix <- NULL
+      set <- function(y) {
+        x <<- y
+        inverseMatrix <<- NULL
+      }
+      get <- function() x
+      setinverse <- function(inverse) inverseMatrix <<- inverse
+      getinverse <- function() inverseMatrix
+      list(set = set, 
+           get = get,
+           setinverse = setinverse,
+           getinverse = getinverse)
 }
+
+# Now if we follow the same functionality with utilizing the inverse function rather than the mean function
+# we can accomplish our goal which is to utilize closures (<<- operator) to make a mutable state in R
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      ## Return a matrix that is the inverse of 'x'
+  
+      inverseMatrix <- x$getinverse()
+      if (!is.null(inverseMatrix)) {
+        message("getting cached data")
+        return(i)
+      }
+      data <- x$get()
+      inverseMatrix <- solve(data, ...)
+      x$setinverse(inverseMatrix)
+      inverseMatrix
 }
+
+# This is a test matrix we will utilize with these formulas
+# NOTE: This must have an inverse matrix for this to be able to run
+A <- matrix(c(1,2,-1,2,1,2,-1,2,1),3,3)
+
+
+inverseA <- makeCacheMatrix(A)
+# this will make a list item which is cached and then put said matrix into the list with each functionality
+# i.e set the matrix in set, then get the matrix in get, and so forth with the set inverse function and getting result of said inverse in get inverse item
+
+cacheSolve(inverseA) 
+# this should get grab the inverse if it is already calculated otherwise it will calculate it and spit out the results for you
